10715537b8818a8edb542cafc7b5064f025a0f4e
Marie Sheela JOSEPH LOUIS SELVARAJ <sheelajoseph200@gmail.com>
2023-05-08 16:35:59 +0200

    added useState

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
+import { useState } from "react"
 import { contacts } from "./data"
+import './App.css'
 function App() {
+    const [current, setcurrent] = useState(contacts[0])
+    const [message, setMessage] = useState('')
     const handleSubmit = (e) => {
         e.preventDefault()
+        setTimeout(() => alert(`'${message}'sent to ${current} at ${new Date()}}`), 4000)
     }
     return (
-        <div>
-            <form onSubmit={(e) => handleSubmit(e)}>
-                <div>
-                    <select>
-                        {contacts.map((c) => <option>{c}</option>)}
-                    </select>
-                </div>
-                <textarea></textarea>
-                <div>
-                    <button>SEND</button>
-                </div>
-            </form>
+        <div className="app">
+            <div>
+                <h1>CHAT TIME</h1>
+                <form onSubmit={(e) => handleSubmit(e)}>
+                    <div>
+                        <label htmlFor="contact">To</label>
+                        <br></br>
+                        <select
+                            id="contact"
+                            value={current}
+                            onChange={(e) => setcurrent(e.target.value)}
+                        >
+                            {contacts.map((c) => <option
+                                key={c}
+                                value={c}
+                            >{c}</option>)}
+                        </select>
+                    </div>
+                    <label htmlFor="message">Message</label>
+                    <br></br>
+                    <textarea
+                        id="message"
+                        onChange={(e) => setMessage(e.target.value)}
+                    >{message}</textarea>
+                    <div className="app__send">
+                        <button>SEND</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
